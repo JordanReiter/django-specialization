ee9835a710c9a5d237bbc164abbab830909133fb
Emil Eriksson <asheidan@gmail.com>
2012-08-15 16:22:23 +0200

    Easyinstall info

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+version = __import__('specialization').__version__
+
+setup(
+    name = 'django-specialization',
+    version = version,
+    description = 'Primitive app for dynamically load templates from different folders based on hostname',
+    author = 'Emil Eriksson, CodeMill AB',
+    author_email = 'opensource@codemill.se',
+    url = 'http://github.com/asheidan/django-specialization',
+    install_requires = [
+        'django>=1.3',
+    ],
+)
